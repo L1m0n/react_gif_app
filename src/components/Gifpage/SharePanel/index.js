a1e661b8bae8b4ea52c65d8829f7cbcb04f1e3c4
Vadym Leonov <l1m0n3zon@gmail.com>
2017-02-03 00:25:39 +0200

    Added single gif preview

--- src/components/Gifpage/SharePanel/index.js
+++ src/components/Gifpage/SharePanel/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 
 const SharePanel = (props) => {
+
+    let share,
+        copy;
+    share = () => {
+        return open('https://www.facebook.com/sharer/sharer.php?u=' + props.fb, 'Share on Facebook', 'width=600px, height=500px');
+    };
+
+    copy = (e) => {
+        e.target.querySelector('input').select();
+        document.execCommand('copy');
+    };
+
     return (
         <div className="share-panel">
-            <button className="share-panel__button">
+
+            <span
+                onClick={(e)=>{copy(e)}}
+                className="share-panel__button share-panel__button--copy"
+            >
                 Copy link
-            </button>
-            <button className="share-panel__button">
+                <input className="share-panel__input" readOnly type="text" value={props.url} />
+            </span>
+            <a
+                href={props.url}
+                download={'gif-hub'+1+'.gif'}
+                target="_blank"
+                className="share-panel__button share-panel__button--download"
+            >
                 Download
-            </button>
-            <button className="share-panel__button">
+            </a>
+            <span
+                onClick={()=>{share()}}
+                className="share-panel__button share-panel__button--fb"
+            >
                 Share on facebook
-            </button>
+            </span>
         </div>
     )
 };
